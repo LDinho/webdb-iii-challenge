338973c07d3175e9f14798def80eed7856b840ad
LD <LDinho@users.noreply.github.com>
2019-05-30 23:16:51 -0400

    add database initial knex migration

---
+++ migrations/20190530224643_initial_table.js
@@ -0,0 +1,33 @@
+// implement changes to schema
+exports.up = async function(knex) {
+  await knex.schema.createTable('cohorts', (tbl) => {
+    tbl.increments('id');
+    tbl
+      .string('name')
+      .unique()
+      .notNullable();
+  });
+
+  await knex.schema.createTable('students', (tbl) => {
+    tbl.increments('id')
+    tbl
+      .string('name')
+      .unique()
+      .notNullable();
+    tbl
+      .integer('cohort_id')
+      .references('id')
+      .inTable('cohorts')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+      .notNullable();
+  });
+
+};
+
+// Undo changes
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('students');
+  await knex.schema.dropTableIfExists('cohorts');
+
+};
